adcf9b9528b5f699c29966b47a4203a4321a27c1
Pedro Alves <alves.pedro0@gmail.com>
2017-07-18 14:36:57 +0200

    Added standard and custom validation

--- JSF/ps-jsf-musicshop/src/main/java/no/jsf/musicshop/UserDetails.java
+++ JSF/ps-jsf-musicshop/src/main/java/no/jsf/musicshop/UserDetails.java
@@ -1,5 +1,9 @@
 package no.jsf.musicshop;
 
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
 import javax.faces.view.ViewScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
@@ -24,7 +28,16 @@ public class UserDetails implements Serializable {
         user = userManager.isSignedIn() ? userManager.getCurrentUser() : new User();
     }
 
+    public void validateEmail(FacesContext context, UIComponent component, Object value) {
+        String emailAddress = (String) value;
+        if (emailAddress != null && !emailAddress.equals("") && !emailAddress.contains("@")) {
+            throw new ValidatorException(new FacesMessage("Please enter a valid e-mail address."));
+        }
+    }
+
+
     public String submit() {
         return userManager.save(user);
     }
 }
+
