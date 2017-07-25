2e4187e948487d5fd6b4504de2a6b823f6470b4c
Pedro Alves <alves.pedro0@gmail.com>
2017-07-25 14:49:22 +0200

    Added primefaces to project

---
+++ JSF/ps-jsf-musicshop/src/main/java/no/jsf/musicshop/PrimeFaces.java
@@ -0,0 +1,35 @@
+package no.jsf.musicshop;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+
+@Named
+@SessionScoped
+public class PrimeFaces implements Serializable {
+	
+	 
+	private static final long serialVersionUID = 1L;
+	
+	private Date date;
+	private String redirectURL = "https://github.com/primefaces/primefaces";
+	
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	public String getRedirectURL() {
+		return redirectURL;
+	}
+
+	public void setRedirectURL(String redirectURL) {
+		this.redirectURL = redirectURL;
+	}
+	
+}
