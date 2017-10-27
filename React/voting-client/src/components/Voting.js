b90706352621440d9662cfdd2465731afe6f9f8c
pedroaalves <pedro.alves@innovatec.no>
2017-10-27 16:37:42 +0200

    React

--- React/voting-client/src/components/Voting.js
+++ React/voting-client/src/components/Voting.js
@@ -64,3 +64,28 @@ export default React.createClass({
 });
 */
 
+
+/*
+TODO: Router example
+
+// npm install --save react-router@2.0.0
+
+src/index.jsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Router, Route, hashHistory} from 'react-router';
+import App from './components/App';
+import Voting from './components/Voting';
+import Results from './components/Results';
+
+const routes = <Route component={App}>
+  <Route path="/results" component={Results} /> //http://localhost:8080/#/results,
+  <Route path="/" component={Voting} />         
+</Route>;
+
+ReactDOM.render(
+  <Router history={hashHistory}>{routes}</Router>,
+  document.getElementById('app')
+);
+
+*/
