50c48b5ecfd70ef5da7969bdef8102de97f0b945
Pedro Alves <alves.pedro0@gmail.com>
2018-02-20 17:32:51 +0100

    FrontApp

--- Angular/redux-architecture/ngrx-store-effects-app-master/ngrx-store-effects-app-master/src/app/app.module.ts
+++ Angular/redux-architecture/ngrx-store-effects-app-master/ngrx-store-effects-app-master/src/app/app.module.ts
@@ -1,25 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { 
-  Routes, 
-  RouterModule 
-} from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-import { 
-  StoreRouterConnectingModule, 
-  RouterStateSerializer
+import {
+  StoreRouterConnectingModule,
+  RouterStateSerializer,
 } from '@ngrx/router-store';
-import { 
-  StoreModule, 
-  MetaReducer 
-} from '@ngrx/store';
+import { StoreModule, MetaReducer } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 
-import { 
-  reducers, 
-  CustomSerializer 
-} from './store';
+import { reducers, effects, CustomSerializer } from './store';
 
 // not used in production
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
@@ -53,11 +44,11 @@ export const ROUTES: Routes = [
     BrowserAnimationsModule,
     RouterModule.forRoot(ROUTES),
     StoreModule.forRoot(reducers, { metaReducers }),
-    EffectsModule.forRoot([]),
+    EffectsModule.forRoot(effects),
     StoreRouterConnectingModule,
     environment.development ? StoreDevtoolsModule.instrument() : [],
   ],
-  providers: [{ provide: RouterStateSerializer, useClass: CustomSerializer}],
+  providers: [{ provide: RouterStateSerializer, useClass: CustomSerializer }],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
 })
--- Angular/redux-architecture/ngrx-store-effects-app-master/ngrx-store-effects-app-master/src/app/store/reducers/index.ts
+++ Angular/redux-architecture/ngrx-store-effects-app-master/ngrx-store-effects-app-master/src/app/store/reducers/index.ts
@@ -1,50 +1,44 @@
-import { 
-    ActivatedRouteSnapshot, 
-    RouterStateSnapshot, 
-    Params 
-} from '@angular/router';
-
-import { 
-    createFeatureSelector,
-    ActionReducerMap     
-} from '@ngrx/store'
-
-import * as fromRouter from '@ngrx/router-store';
-
-export interface RouterStateUrl {
+import {
+    ActivatedRouteSnapshot,
+    RouterStateSnapshot,
+    Params,
+  } from '@angular/router';
+  import { createFeatureSelector, ActionReducerMap } from '@ngrx/store';
+  
+  import * as fromRouter from '@ngrx/router-store';
+  
+  export interface RouterStateUrl {
     url: string;
     queryParams: Params;
     params: Params;
-}
-
-// specification from Angular 
-export interface State {
+  }
+  
+  // specification from Angular 
+  export interface State {
     routerReducer: fromRouter.RouterReducerState<RouterStateUrl>;
-}
-
-export const reducers : ActionReducerMap<State> = {
+  }
+  
+  export const reducers: ActionReducerMap<State> = {
     routerReducer: fromRouter.routerReducer,
-};
-
-export const getRouterState = createFeatureSelector<
-    fromRouter.RouterReducerState<RouterStateUrl>>(
-        'routerReduce'
-);
-
-// RouterStateSnapshot extends Tree<ActivatedRouteSnapshot> where is located all params
-export class CustomSerializer 
-    implements fromRouter.RouterStateSerializer<RouterStateUrl>{
+  };
+  
+  export const getRouterState = createFeatureSelector<
+    fromRouter.RouterReducerState<RouterStateUrl>
+  >('routerReducer');
+  
+  // RouterStateSnapshot extends Tree<ActivatedRouteSnapshot> where is located all params
+  export class CustomSerializer
+    implements fromRouter.RouterStateSerializer<RouterStateUrl> {
     serialize(routerState: RouterStateSnapshot): RouterStateUrl {
-        const { url } = routerState; // <=> const url = routerState.url from es6
-        const { queryParams } = routerState.root;
-
-        let state: ActivatedRouteSnapshot = routerState.root;
-        while(state.firstChild) {
-            // here is angular router (not ngrx router)
-            state = state.firstChild;
-        }
-        const { params } = state ;
-
-        return { url, queryParams, params };
+      const { url } = routerState;
+      const { queryParams } = routerState.root;
+  
+      let state: ActivatedRouteSnapshot = routerState.root;
+      while (state.firstChild) {
+        state = state.firstChild;
+      }
+      const { params } = state;
+  
+      return { url, queryParams, params };
     }
-}
+  }
