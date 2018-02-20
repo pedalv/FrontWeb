9e0df171482331e64aa3e95aa0762944ac2c266b
Pedro Alves <alves.pedro0@gmail.com>
2018-02-20 10:46:40 +0100

    FrontApp

--- Angular/redux-architecture/ngrx-store-effects-app-master/ngrx-store-effects-app-master/src/products/containers/product-item/product-item.component.ts
+++ Angular/redux-architecture/ngrx-store-effects-app-master/ngrx-store-effects-app-master/src/products/containers/product-item/product-item.component.ts
@@ -16,7 +16,7 @@ import { Topping } from '../../models/topping.model';
       class="product-item">
       <pizza-form
         [pizza]="pizza$ | async"
-        [toppings]="toppings"
+        [toppings]="toppings$ | async"
         (selected)="onSelect($event)"
         (create)="onCreate($event)"
         (update)="onUpdate($event)"
@@ -31,7 +31,7 @@ import { Topping } from '../../models/topping.model';
 export class ProductItemComponent implements OnInit {
   pizza$: Observable<Pizza>;
   visualise: Pizza;
-  toppings: Topping[];
+  toppings$: Observable<Topping[]>;
 
   constructor(
     private store: Store<fromStore.ProductsState>
@@ -45,6 +45,7 @@ export class ProductItemComponent implements OnInit {
   ngOnInit() {
     this.store.dispatch(new fromStore.LoadToppings());
     this.pizza$ = this.store.select(fromStore.getSelectedPizza);
+    this.toppings$ = this.store.select(fromStore.getAllToppings);
 
 
     /*
---
+++ Angular/redux-architecture/ngrx-store-effects-app-master/ngrx-store-effects-app-master/src/products/store/selectors/toppings.selectors.ts
@@ -0,0 +1,40 @@
+import { createSelector } from '@ngrx/store';
+
+import * as fromRoot from '../../../app/store';
+import * as fromFeature from '../reducers';
+import * as fromToppings from '../reducers/toppings.reducer';
+
+/*
+this is the data structured
+{
+    product: {
+        pizzas: {},
+        toppings: {
+            entities: {}
+        }
+    }
+}
+*/
+
+export const getToppingsState = createSelector(
+    fromFeature.getproductState,
+    (state: fromFeature.ProductsState) => state.toppings
+);
+
+export const getToppingsEntities = createSelector(
+    getToppingsState,
+    fromToppings.getToppingEntities
+);
+
+export const getAllToppings = createSelector(
+    getToppingsEntities,
+    entities => {
+        return Object.keys(entities)
+            .map( id => entities[parseInt(id,10)] );
+    }
+)
+
+export const getToppingsLoaded = createSelector(
+    getToppingsState,
+    fromToppings.getToppingLoading
+);
