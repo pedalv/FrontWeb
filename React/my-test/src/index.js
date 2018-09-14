ef66d70ed200a3d23912239894de9bb784c3cb75
Pedro Alves <Pedro.Alves@apotek1.no>
2018-09-14 15:08:52 +0200

    React test

--- React/my-test/src/index.js
+++ React/my-test/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
+import AppDate from './components/AppDate/AppDate';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<AppDate />, document.getElementById('root1'));
+
 registerServiceWorker();
