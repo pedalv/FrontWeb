5820a50c70eb6156808c65f798b81c68c27f2ce4
Pedro Alves <Pedro.Alves@apotek1.no>
2018-09-17 11:15:42 +0200

    Webpack4

--- Webpack/webpack-4-quickstart/src/index.js
+++ Webpack/webpack-4-quickstart/src/index.js
@@ -1,7 +1,10 @@
 //import App from "./App";
 import App from "./components/App/App";
 import style from "./main.css";
+import registerServiceWorker from './registerServiceWorker';
 
 if (process.env.NODE_ENV !== 'production') {
 	console.log('Looks like we are in development mode!');
 }
+
+registerServiceWorker();
