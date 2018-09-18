5e8865eaec887114096af95afa1627355b515fe5
Pedro Alves <Pedro.Alves@apotek1.no>
2018-09-18 10:13:47 +0200

    Recat ts

---
+++ React/my-app-ts/src/components/Kunde/Kunde.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './Kunde.css';
+
+class Kunde extends React.Component {
+  public render() {
+    return (
+      <div className="Kunde">
+        <p>Apotek1 here!</p>
+      </div>
+    );
+  }
+}
+
+export default Kunde;
--- React/my-app-ts/src/index.tsx
+++ React/my-app-ts/src/index.tsx
@@ -1,8 +1,14 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App/App';
+import Kunde from './components/Kunde/Kunde';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(
+    <Kunde />, 
+    document.getElementById("kunde") as HTMLElement
+);
 
 ReactDOM.render(
     <App />, 
