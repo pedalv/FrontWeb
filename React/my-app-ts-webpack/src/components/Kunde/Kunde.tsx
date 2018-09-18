5fc3338d14fdbdfa9eb47cb4be22b34f697e3eae
Pedro Alves <Pedro.Alves@apotek1.no>
2018-09-18 16:13:33 +0200

    react ts webpack

---
+++ React/my-app-ts-webpack/src/components/Kunde/Kunde.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './Kunde.css';
+
+class Kunde extends React.Component {
+  public render() {
+    return (
+      <div className="Kunde">
+        <h2>Apotek1 here!</h2>
+      </div>
+    );
+  }
+}
+
+export default Kunde;
