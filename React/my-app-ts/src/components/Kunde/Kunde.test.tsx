53e68d13aac885e883c6f3f388f6240ce9eca50b
Pedro Alves <Pedro.Alves@apotek1.no>
2018-09-18 10:17:18 +0200

    Recat ts

--- React/my-app-ts/src/components/Kunde/Kunde.test.tsx
+++ React/my-app-ts/src/components/Kunde/Kunde.test.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+import Kunde from './Kunde';
 
-it('renders app without crashing', () => {
+it('renders kunde without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Kunde />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
